4ce4db9f70329d71d35128cb50b5b8aafd9e1c01
haritsnaufalich <haritsnaufalichs11@gmail.com>
2022-10-06 19:21:10 +0700

    Routing

--- mvc/app/core/App.php
+++ mvc/app/core/App.php
@@ -2,6 +2,13 @@
 
 class App {
   public function __construct() {
-    echo 'OK!';
+    $url = $this->parseUrl();
+    var_dump($url);
+  }
+
+  public function parseUrl() {
+    if (isset($_GET['url'])) {
+      return $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+    }
   }
 }
