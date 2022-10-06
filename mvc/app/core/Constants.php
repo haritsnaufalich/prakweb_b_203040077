41b1bfae09a09fa845944fc8329ca8b155126176
haritsnaufalich <haritsnaufalichs11@gmail.com>
2022-10-06 21:02:36 +0700

    Database Wrapper

--- mvc/app/core/Constants.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-define('BASEURL', 'http://localhost/prakweb_b_203040077/mvc/public');
--- mvc/app/init.php
+++ mvc/app/init.php
@@ -2,4 +2,5 @@
 
 require_once 'core/App.php';
 require_once 'core/Controller.php';
-require_once 'core/Constants.php';
+require_once 'core/database.php';
+require_once 'config/Config.php';
